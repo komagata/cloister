b0a84352d8a2a3c6bcaa660cda72efbc70e19509
Masaki Komagata <komagata@gmail.com>
2009-10-05 23:24:37 +0900

    fixed title

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ ActionController::Routing::Routes.draw do |map|
   map.sitemap 'sitemap.xml', :controller => 'top', :action => 'sitemap'
   map.docs 'index.:format', :controller => 'docs', :action => 'index', :conditions => {:method => :get}
   map.docs 'index.:format', :controller => 'docs', :action => 'create', :conditions => {:method => :post}
-  map.find_doc 'find', :controller => 'docs', :action => 'index', :conditions => {:method => :get}
+  map.find_doc '/', :controller => 'docs', :action => 'index', :conditions => {:method => :get}
   map.new_doc 'new', :controller => 'docs', :action => 'new', :conditions => {:method => :get}
   map.edit_doc ':id/edit', :controller => 'docs', :action => 'edit', :conditions => {:method => :get}
   map.doc ':id', :controller => 'docs', :action => 'show', :conditions => {:method => :get}
