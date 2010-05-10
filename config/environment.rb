68d7c2e8d05a8b84827ae688dfda1a1b157c3d36
Masaki KOMAGATA <komagata@gmail.com>
2010-05-11 00:52:43 +0900

    update to 2.3.5

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -15,7 +15,7 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem "pagination_scope"
+  config.gem 'pagination_scope'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
@@ -36,12 +36,3 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   config.i18n.default_locale = 'en'
 end
-
-CalendarDateSelect::FORMATS[:japanese] = {
-  :date => "%Y/%m/%d",
-  :time => " %H:%M",
-  :javascript_include => "format_japanese"
-}
-CalendarDateSelect.format = :japanese
-
-ENV['GEM_PATH'] = '/home/komagata/.gems'
--- config/initializers/calendar_date_select.rb
+++
@@ -1,6 +0,0 @@
-CalendarDateSelect::FORMATS[:japanese] = {
-  :date => "%Y/%m/%d",
-  :time => " %H:%M",
-  :javascript_include => "format_japanese"
-}
-CalendarDateSelect.format = :japanese
